c39e0a3e63628a3008bad869a2351927b4216b56
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2020-08-20 15:34:43 +0400

    implemented split feature

--- src/modules/Ad/Pages/components/SplittedInput.js
+++ src/modules/Ad/Pages/components/SplittedInput.js
@@ -1,27 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import { InputAdornment, Grid, IconButton } from '@material-ui/core';
+import Icon from '@mdi/react';
+import { mdiLink, mdiLinkOff } from '@mdi/js';
 
 export default ({ children, splitted: _splitted, networks }) => {
   const [splitted, setSplitted] = useState(_splitted);
   const [renderChildren, setRenderChildren] = useState(children);
-  if (React.Children.count(children) > 1) {
+  if (!React.Children.only(children)) {
     throw 'Children must be only 1';
   }
 
-  useEffect(() => {
-    if (!splitted) return setRenderChildren(children);
+  const splitButton = (
+    <IconButton onClick={() => setSplitted(!splitted)}>
+      <Icon path={splitted ? mdiLink : mdiLinkOff} />
+    </IconButton>
+  );
+
+  const mergeInputs = () => {
+    const singleInput = React.cloneElement(children, {
+      sizes: { xs: 6 },
+      InputProps: {
+        startAdornment: <InputAdornment position='start'>{splitButton}</InputAdornment>,
+      },
+    });
+
+    setRenderChildren(singleInput);
+  };
+  const splitInputs = () => {
     const _chidrenToRender = [];
 
     networks.map((network) => {
-      const child = children;
-      console.log(child, child.props, child.props.sizes);
-      // child.props.sizes = { xs: 6 };
-      // child.props['data-network'] = network;
-
-      _chidrenToRender.push(child);
-      console.log(child);
+      _chidrenToRender.push(
+        React.cloneElement(children, {
+          sizes: { xs: 6 },
+          InputProps: {
+            'data-network': network,
+            startAdornment: <InputAdornment position='start'>{splitButton}</InputAdornment>,
+          },
+        })
+      );
     });
 
     setRenderChildren(_chidrenToRender);
+  };
+
+  useEffect(() => {
+    if (!splitted) {
+      return mergeInputs();
+    } else {
+      return splitInputs();
+    }
   }, [splitted]);
 
   return renderChildren;
