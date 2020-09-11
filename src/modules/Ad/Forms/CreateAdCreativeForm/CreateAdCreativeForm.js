7b1b8441fb1dac8bcd3943d7d46b7f3365bcedea
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2020-09-11 16:34:01 +0400

    added ad creative form

--- src/modules/Ad/Forms/CreateAdCreativeForm/CreateAdCreativeForm.js
+++ src/modules/Ad/Forms/CreateAdCreativeForm/CreateAdCreativeForm.js
@@ -14,64 +14,30 @@ export default (props) => {
   const [bidType, setBidType] = useState('automatic');
 
   return (
-    <form>
-      <Grid container>
-        <Grid item xs={12}>
-          <ExpansionPanel title='Creative Name' subTitle='Enter Creative Name'>
-            <CreativeNameField
-              label='Enter Creative Name'
-              name='name'
-              onChange={(e) => console.log(e.target.value)}
-            />
-          </ExpansionPanel>
-          <ExpansionPanel title='Choose Facebook Page' subTitle='Choose Facebook Page'>
-            <FacebookPageField
-              label='Choose Facebook Page'
-              name='facebook_page_id'
-              onChange={(e) => console.log(e.target.value)}
-            />
-          </ExpansionPanel>
-          <ExpansionPanel title='Choose Facebook Image' subTitle='Choose Facebook Image'>
-            <CreativeFacebookImageField
-              label='Choose Facebook Image'
-              name='media'
-              onChange={(e) => console.log(e.target.value)}
-            />
-          </ExpansionPanel>
-          <ExpansionPanel title='Choose Facebook Headlines' subTitle='Choose Facebook Headlines'>
-            <CreativeHeadlinesField
-              label='Choose Facebook Headlines'
-              name='headlines[]'
-              onChange={(e) => console.log(e.target.value)}
-            />
-          </ExpansionPanel>
-          <ExpansionPanel
-            title='Choose Facebook Description'
-            subTitle='Choose Facebook Description'
-          >
-            <CreativeLongHeadlineField
-              label='Enter Description'
-              name='descriptions[]'
-              onChange={(e) => console.log(e.target.value)}
-            />
-          </ExpansionPanel>
-          <ExpansionPanel title='Primary Text' subTitle='Choose Facebook Primary Text'>
-            <CreativePrimaryTextField
-              label='Enter Primary Text'
-              name='primary_text'
-              onChange={(e) => console.log(e.target.value)}
-            />
-          </ExpansionPanel>
-          <ExpansionPanel title='Display Link' subTitle='Choose Facebook Display Link'>
-            <CreativeDisplayLinkField
-              label='Display Link'
-              name='display_link'
-              onChange={(e) => console.log(e.target.value)}
-            />
-            6
-          </ExpansionPanel>
-        </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <ExpansionPanel title='Creative Name' subTitle='Enter Creative Name'>
+          <CreativeNameField />
+        </ExpansionPanel>
+        <ExpansionPanel title='Choose Facebook Page' subTitle='Choose Facebook Page'>
+          <FacebookPageField />
+        </ExpansionPanel>
+        <ExpansionPanel title='Choose Facebook Image' subTitle='Choose Facebook Image'>
+          <CreativeFacebookImageField />
+        </ExpansionPanel>
+        <ExpansionPanel title='Choose Facebook Headlines' subTitle='Choose Facebook Headlines'>
+          <CreativeHeadlinesField />
+        </ExpansionPanel>
+        <ExpansionPanel title='Choose Facebook Description' subTitle='Choose Facebook Description'>
+          <CreativeLongHeadlineField />
+        </ExpansionPanel>
+        <ExpansionPanel title='Primary Text' subTitle='Choose Facebook Primary Text'>
+          <CreativePrimaryTextField />
+        </ExpansionPanel>
+        <ExpansionPanel title='Display Link' subTitle='Choose Facebook Display Link'>
+          <CreativeDisplayLinkField />
+        </ExpansionPanel>
       </Grid>
-    </form>
+    </Grid>
   );
 };
--- src/modules/Ad/Pages/CreateAdPages/CreateNewAdPage.js
+++ src/modules/Ad/Pages/CreateAdPages/CreateNewAdPage.js
@@ -1,5 +1,6 @@
 import { Button, Grid } from '@material-ui/core';
 import ExpansionPanel from 'components/ExpansionPanel/ExpansionPanel';
+import CreateAdCreativeForm from 'modules/Ad/Forms/CreateAdCreativeForm';
 import CreativeDisplayLinkField from 'modules/Ad/Forms/CreateAdCreativeForm/components/CreativeDisplayLinkField';
 import CreativeFacebookImageField from 'modules/Ad/Forms/CreateAdCreativeForm/components/CreativeFacebookImageField';
 import CreativeHeadlinesField from 'modules/Ad/Forms/CreateAdCreativeForm/components/CreativeHeadlinesField';
@@ -14,61 +15,13 @@ import AdGroupService from 'services/AdGroupService';
 import CampaignService from 'services/CampaignService';
 
 export default (props) => {
-  const {
-    match: {
-      params: { network, campaign: campaignId, adgroup: adgroupId },
-    },
-  } = props;
-
-  const [adgroup, setAdGroup] = useState({
-    name: 'Example Adset Name',
-    type: 'conversions',
-    status: 'active',
-    campaign_id: 23845574488410561,
-    budget: 2,
-    start_date: '2020-09-24',
-    billing_event: 'clicks',
-    end_date: '2020-09-25',
-    optimization_goal: 'link_clicks',
-    bid_amount: 1,
-    bid_strategy: 'cost_cap',
-    targetings: {
-      locations: {
-        countries: ['US', 'GEO'],
-        cities: [
-          '2430536',
-          {
-            key: 3847,
-            radius: 11,
-            distance_unit: 'km',
-          },
-        ],
-        regions: ['3847'],
-        location_type: 'recent',
-      },
-      gender: 'male',
-      age_max: 44,
-      age_min: 32,
-      parental_status: 'parents',
-      targeting_expansion: 50,
-      audiences: [23845468848830561],
-    },
-  });
   const [ad, setAd] = useState();
   const [existingCampaign, setExistingCampaign] = useState(null);
+  const [existingAdGroup, setExistingAdGroup] = useState(null);
   const [networks, setNetworks] = useState(['facebook', 'google']);
   const campaignFormRef = useRef();
   const adGroupFormRef = useRef();
-
-  const formSubmitted = (e) => {
-    e.preventDefault();
-    const form = new FormData(e.target);
-    console.log(form);
-  };
-
-  const isNetworkEnabled = (n) => {
-    return networks.indexOf(n) !== -1;
-  };
+  const adCreativeFormRef = useRef();
 
   const existingCampaignChoosed = (c) => {
     setExistingCampaign(c);
@@ -104,7 +57,12 @@ export default (props) => {
         <Button variant='contained' onClick={createAd}>
           Submit
         </Button>
-        <ExpansionPanel expanded transparent titleBefore='Adset Configuration' title='Adset'>
+        <ExpansionPanel titleBefore='Ad Creative Configuration' title='Ad Creative'>
+          <form ref={adCreativeFormRef}>
+            <CreateAdCreativeForm adgroup={existingAdGroup} />
+          </form>
+        </ExpansionPanel>
+        <ExpansionPanel transparent titleBefore='Adset Configuration' title='Adset'>
           <form ref={adGroupFormRef}>
             <CreateAdGroupForm campaign={existingCampaign} />
           </form>
@@ -116,41 +74,6 @@ export default (props) => {
               onNetworkChange={onNetworkChange}
             />
           </form>
-        </ExpansionPanel>
-
-        <ExpansionPanel titleBefore='Ad Creative Configuration' title='Ad Creative'>
-          {/* <CreateAdCreativeForm networks={[network]} context={ad} setContext={setAd} /> */}
-          <Grid container>
-            <Grid item xs={12}>
-              <ExpansionPanel title='Creative Name' subTitle='Enter Creative Name'>
-                <CreativeNameField />
-              </ExpansionPanel>
-              <ExpansionPanel title='Choose Facebook Page' subTitle='Choose Facebook Page'>
-                <FacebookPageField />
-              </ExpansionPanel>
-              <ExpansionPanel title='Choose Facebook Image' subTitle='Choose Facebook Image'>
-                <CreativeFacebookImageField />
-              </ExpansionPanel>
-              <ExpansionPanel
-                title='Choose Facebook Headlines'
-                subTitle='Choose Facebook Headlines'
-              >
-                <CreativeHeadlinesField />
-              </ExpansionPanel>
-              <ExpansionPanel
-                title='Choose Facebook Description'
-                subTitle='Choose Facebook Description'
-              >
-                <CreativeLongHeadlineField />
-              </ExpansionPanel>
-              <ExpansionPanel title='Primary Text' subTitle='Choose Facebook Primary Text'>
-                <CreativePrimaryTextField />
-              </ExpansionPanel>
-              <ExpansionPanel title='Display Link' subTitle='Choose Facebook Display Link'>
-                <CreativeDisplayLinkField />
-              </ExpansionPanel>
-            </Grid>
-          </Grid>
         </ExpansionPanel>
       </Grid>
     </Grid>
