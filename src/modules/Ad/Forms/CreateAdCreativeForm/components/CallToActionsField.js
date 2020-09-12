274964622058196a6a4c2fb77634e0a2b72a4a70
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2020-09-12 21:31:52 +0400

    added google call to action texts

--- src/modules/Ad/Forms/CreateAdCreativeForm/components/CallToActionsField.js
+++ src/modules/Ad/Forms/CreateAdCreativeForm/components/CallToActionsField.js
@@ -17,15 +17,18 @@ export default (props) => {
   ];
 
   const googleOptions = [
-    { value: 'open_link', name: 'Open Link' },
-    { value: 'like_page', name: 'Like Page' },
-    { value: 'shop_now', name: 'Shop Now' },
-    { value: 'play_game', name: 'Play Game' },
-    { value: 'learn_more', name: 'Learn More' },
-    { value: 'watch_video', name: 'Watch Video' },
-    { value: 'apply_now', name: 'Apply Now' },
-    { value: 'message_page', name: 'Message Page' },
-    { value: 'see_more', name: 'See More' },
+    { name: 'Apply Now', value: 'Apply Now' },
+    { name: 'Book Now', value: 'Book Now' },
+    { name: 'Contact Us', value: 'Contact Us' },
+    { name: 'Download', value: 'Download' },
+    { name: 'Learn More', value: 'Learn More' },
+    { name: 'Install', value: 'Install' },
+    { name: 'Visit Site', value: 'Visit Site' },
+    { name: 'Shop Now', value: 'Shop Now' },
+    { name: 'Sign Up', value: 'Sign Up' },
+    { name: 'Get Quote', value: 'Get Quote' },
+    { name: 'Subscribe', value: 'Subscribe' },
+    { name: 'See More', value: 'See More' },
   ];
 
   return (
